d09428bf31cb3d318ffa321d996fa8b375001324
Gaurav Chitwaan <gaurav198.rr@gmail.com>
2023-07-07 23:30:32 +0530

    access token and refresh token implemented

--- routes/users.js
+++ routes/users.js
@@ -41,4 +41,22 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.post('/refresh', async (req, res) => {
+    const {refreshToken} = req.body;
+    if(!refreshToken) {
+      return res.sendStatus(401);
+    }
+    const user = await User.findOne({refreshToken});
+    if(!user) {
+      return res.sendStatus(403);
+    }
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+      if(err) {
+        return res.sendStatus(403);
+      }
+      const accessToken = jwt.sign({id: user.id, username: user.username}, process.env.JWT_SECRET, {expiresIn: '1m'});
+      res.send({accessToken});
+    });
+  });  
+
 module.exports = router;
