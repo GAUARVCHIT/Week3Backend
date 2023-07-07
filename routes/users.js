af877673fdc370d35c1d893fb2b62d0f89fc3780
Gaurav Chitwaan <gaurav198.rr@gmail.com>
2023-07-07 23:17:32 +0530

    user login route implemented

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
@@ -8,10 +9,10 @@ const bcrypt = require('bcrypt');
 const router = express.Router();
 router.post('/signup', async (req, res) => {
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        // const hashedPassword = await bcrypt.hash(req.body.password, 10);
         const user = new User({
             username: req.body.username,
-            password: hashedPassword
+            password: req.body.password
         });
         await user.save();
         res.status(201).json({ message: "User created successfully" });
@@ -20,5 +21,24 @@ router.post('/signup', async (req, res) => {
     }
 }
 );
+
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+        const accessToken = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '15m' });
+        const refreshToken = jwt.sign({ id: user.id, username: user.username }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+
+        user.refreshToken = refreshToken;
+        await user.save();
+        res.json({ accessToken, refreshToken, message: "Logged in successfully" });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 module.exports = router;
